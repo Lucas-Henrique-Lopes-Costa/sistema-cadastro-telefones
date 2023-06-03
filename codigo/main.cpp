09f1083f403e7f6615b6b056d4a91c9afed50b92
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-06-03 14:26:31 -0300

    base de dados e código gpt

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -1,6 +1,12 @@
 /*
   sistema de cadastro (com recursos para busca e alteração de dados) com armazenamento dos dados em arquivos binários
-
-
 */
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  cout << "Hello World" << endl;
+
+  return ;
+}
